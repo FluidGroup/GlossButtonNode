cc609f887ed3fffdbbc01ecd7201e34ff114cf6e
Muukii <muukii.app@gmail.com>
2022-07-02 19:12:58 +0900

    Patch

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+  name: "GlossButtonNode",
+  platforms: [
+    .iOS(.v11),
+  ],
+  products: [
+    .library(name: "GlossButtonNode", targets: ["GlossButtonNode"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/FluidGroup/Texture.git", branch: "spm"),
+    .package(url: "https://github.com/FluidGroup/TextureSwiftSupport.git", branch: "main"),
+  ],
+  targets: [
+    .target(
+      name: "GlossButtonNode", 
+      dependencies: [
+        .product(name: "AsyncDisplayKit", package: "Texture"),
+        .product(name: "TextureSwiftSupport", package: "TextureSwiftSupport")
+      ],
+      path: "GlossButtonNode"
+    ),
+  ],
+  swiftLanguageVersions: [.v5]
+)
